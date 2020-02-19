119ebb83e5cf2200326cd03d90be12b073a8485a
Mikkel Anderson <s184230@student.dtu.dk>
2020-02-19 15:36:10 +0100

    See if server is live on localhost

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/live', function(req, res, next) {
+  res.send('YES I AM LIVE');
+});
+
 module.exports = router;
