6ab49070e7734b77a317e9f4702ded9c20829c97
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2020-03-25 23:04:04 +0100

    Small useful cookie methods

--- public/scripts/modules/Utility.js
+++ public/scripts/modules/Utility.js
@@ -1,7 +1,14 @@
 export function getCookieValue(cookieName) {
     let cookies = document.cookie.split(';');
-}
+    for (var i = 0; i < cookies.length; i++) {
+    	let cookie = cookies[i].split('=');
+    	if (cookieName === cookie[0]) {
+    		return cookie[1];
+    	}
+    }
 
 export function getResolutionFromCookie(cookieName) {
     let value = getCookieValue(cookieName);
+    let components = value.split('x'); 
+    return {res_x:components[0],res_y:components[1]};
 }
