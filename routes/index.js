b23b24845a889d133c6dc678ed04599a44f7496c
Laura <s184234@student.dtu.dk>
2020-04-06 21:37:54 +0200

    implemented a multi-room chat (to build upon)

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,69 @@
 var express = require('express');
+
+
 var router = express.Router();
+require('dotenv').config();
+var url = process.env.MONGO_DB;
+var MongoClient = require('mongodb').MongoClient;
+var dbName = process.env.MONGO_DBO;
+var collection = process.env.MONGO_COLLECTION_USERS;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sprouts' });
+/**
+ * @namespace routes
+ * GET home page
+ */
+router.get('/', function (req, res, next) {
+    res.render('chat', {title: 'Sprouts'});
 });
 
-router.get('/live', function(req, res, next) {
-  res.send('YES I AM LIVE');
+router.get('/live', function (req, res, next) {
+    res.send('YES I AM LIVE');
 });
 
-router.get('/game', function(req, res, next) {
-  res.render('game', { title: 'Play Sprouts!' });
+router.get('/game', function (req, res, next) {
+    res.render('game', {title: 'Play Sprouts!'});
 });
 
-router.get('/customize', function(req, res, next) {
-  res.render('customize', { title: 'Customize your game' });
+router.get('/customize', function (req, res, next) {
+    res.render('customize', {title: 'Customize your game'});
 });
 
-router.get('/scoreboard', function(req, res, next) {
-  res.render('scoreboard', { title: 'High Scores' });
+/*app.on('connection', function (socket) {
+    console.log("connection")
+});*/
+
+
+/**
+ * @memberOf routes
+ * GET scores from db
+ */
+router.get('/scoreboard', function (req, res, next) {
+    let results_from_mongo = [];
+
+    MongoClient.connect(url)
+        .then(function (client) {
+            const db = client.db(dbName);
+            var cursor = db.collection(collection).find({});
+
+            function iterateFunc(doc) {
+                results_from_mongo.push(doc); // adds scores to array
+            }
+
+            function errorFunc(error) {
+                console.log(error);
+            }
+
+            cursor.forEach(iterateFunc, errorFunc);
+
+            client.close().then(r => {
+                res.render('scoreboard', {"results": results_from_mongo}); // renders scoreboard with data
+            });
+        })
+        .catch(function (err) {
+            res.render('scoreboard', {"results": results_from_mongo}); // renders scoreboard with no data
+            console.log(err);
+        })
+
 });
 
 module.exports = router;
