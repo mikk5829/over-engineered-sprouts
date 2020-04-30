b3986fa371182bc70b800dd2b9778b04ab781175
Wictor <wictor.jensen@gmail.com>
2020-04-30 12:55:44 +0200

    No longer crosses points

--- public/scripts/modules/SproutWorld.js
+++ public/scripts/modules/SproutWorld.js
@@ -399,8 +399,12 @@ export class SproutWorld {
                     ray.legal = true;
                     for (let p of this.lineGroup.children){
                         if (n !== n1 && !n1.explored && p.getCrossings(ray).length === 0){
-                            ray.strokeColor = "red";
-                            ray.opacity = 0.1;
+                            for (let v of this.points) {
+                                if (v === p1 || v === p2 || ray.getIntersections(v).length === 0){
+                                    ray.strokeColor = "red";
+                                    ray.opacity = 0.1;
+                                }
+                            }
                         } else {
                             ray.remove();
                             ray.legal = false;
